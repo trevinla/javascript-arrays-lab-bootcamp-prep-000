f3cb1adcdbdda2bcc77970e8bb0ce10a3d66113d
Trevor Seitz <trevinla@gmail.com>
2017-09-25 23:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
   var kittens = ["Milo", "Otis", "Garfield"];
-  kittens.push("Ralph");
+  kittens.shift("Ralph");
   return kittens;
 }
